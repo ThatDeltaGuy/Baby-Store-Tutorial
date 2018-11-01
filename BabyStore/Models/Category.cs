9a2b5f51acd648eeee4d463f90eccd673b38f904
Alexander Lund <alexanderlund@hotmail.co.uk>
2018-11-01 17:12:38 +0000

    added validation to models

--- BabyStore/Models/Category.cs
+++ BabyStore/Models/Category.cs
@@ -6,6 +6,9 @@ namespace BabyStore.Models
     public class Category
     {
         public int ID { get; set; }
+        [Required(ErrorMessage ="The Category name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a category name between 3 and 50 charactors in length")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage ="Please enter a name beginning with a capital letter containing only letters and spaces")]
         [Display(Name = "Catagory Name")]
         public string Name { get; set; }
         public virtual ICollection<Product> Products { get; set; }
--- BabyStore/Models/Product.cs
+++ BabyStore/Models/Product.cs
@@ -6,8 +6,20 @@ namespace BabyStore.Models
     public partial class Product
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "The product name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a product name between 3 and 50 charactors in length")]
+        [RegularExpression(@"^[a-zA-Z0-9'-'\s]*$", ErrorMessage = "Please enter a product name containing only letters and numbers")]
         public String Name { get; set; }
+        [Required(ErrorMessage = "The product description cannot be blank")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Please enter a product description between 10 and 200 charactors in length")]
+        [RegularExpression(@"^[,;a-zA-Z0-9'-'\s]*$", ErrorMessage = "Please enter a product name containing only letters and numbers")]
+        [DataType(DataType.MultilineText)]
         public String Description { get; set; }
+        [Required(ErrorMessage = "The product price cannot be blank")]
+        [Range(0.10, 10000, ErrorMessage = "Please enter a product price between 0.10 and 10000")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [RegularExpression("[0-9]+(\\.[0-9][0-9]?)?",ErrorMessage ="Price must havce upto 2 decimal places")]
         public decimal Price { get; set; }
         public int? CategoryID { get; set; }
         public virtual Category Category { get; set; }
